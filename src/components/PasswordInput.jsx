96dd8076a04b21bd0c21c4f8c9e49c6e1e285a84
sydomori <sydneyomori@gmail.com>
2026-05-10 03:01:26 +0300

    add functionality

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,16 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    function handleChange(){
+        console.log("Entering password...")
+    }
     return(
-        <></>
+        <>
+        <input type="password"
+        placeholder="Enter your password"
+        onChange={handleChange}
+        ></input>
+        </>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,19 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+    function handleEnter(){
+        console.log("Mouse Entering")
+    }
+    function handleLeave(){
+        console.log("Mouse Exiting")
+    }
     return(
-        <></>
+        <>
+        <button type="submit"
+        onMouseEnter={handleEnter}
+        onMouseLeave={handleLeave}
+        >Submit Password</button>
+        </>
     )
 }
 
